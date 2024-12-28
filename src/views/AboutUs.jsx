0382449cefe3dec5d4cb208df7d289efc73a96a5
Mabsas <139257914+Mabsas@users.noreply.github.com>
2024-12-28 18:26:41 +0600

    Added About page

---
+++ src/views/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>
+        Welcome to our Cat Adoption website! We are dedicated to helping cats find loving homes and 
+        connecting pet lovers with their ideal feline companions.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
